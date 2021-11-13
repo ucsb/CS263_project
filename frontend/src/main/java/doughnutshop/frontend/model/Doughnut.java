5e33a9d060fae17c179ebea3e12f78f5d80aad4d
Iris Moini <azadeh@ucsb.edu>
2021-11-12 16:40:57 -0800

    added doughnut model and doughnut inventory model to frontend service

---
+++ frontend/src/main/java/doughnutshop/frontend/model/Doughnut.java
@@ -0,0 +1,37 @@
+package doughnutshop.frontend.model;
+
+public class Doughnut {
+    private String name;
+    private double price;
+    private int inventory;
+
+    public Doughnut(String name, double price, int inventory) {
+        this.name = name;
+        this.price = price;
+        this.inventory = inventory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+}
