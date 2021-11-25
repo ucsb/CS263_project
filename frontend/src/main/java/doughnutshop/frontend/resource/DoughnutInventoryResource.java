ffeeea3adb3dfcbb63129cc696b1521154fee7d5
Iris Moini <azadeh@ucsb.edu>
2021-11-24 22:32:06 -0800

    modified doughnut view

--- frontend/src/main/java/doughnutshop/frontend/resource/DoughnutInventoryResource.java
+++ frontend/src/main/java/doughnutshop/frontend/resource/DoughnutInventoryResource.java
@@ -1,10 +1,13 @@
 package doughnutshop.frontend.resource;
 
+import doughnutshop.frontend.model.Doughnut;
 import doughnutshop.frontend.model.DoughnutInventory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doughnuts")
@@ -13,9 +16,16 @@ public class DoughnutInventoryResource {
     private RestTemplate restTemplate;
 
     @RequestMapping("/")
-    public DoughnutInventory getDoughnutInventory(){
-        DoughnutInventory doughnutInventory = restTemplate.getForObject("http://inventory-service/doughnuts/inventory", DoughnutInventory.class);
-        return doughnutInventory;
+    public String getDoughnutInventory(){
+        DoughnutInventory doughnutInventory = restTemplate.getForObject("http://inventory-service-database/doughnuts/orderByPrice", DoughnutInventory.class);
+        List<Doughnut> doughnuts = doughnutInventory.getDoughnutsInventory();
+        String doughnutView = "";
+        for(Doughnut doughnut : doughnuts){
+            doughnutView += "Name: " + doughnut.getName() + "<br>"  + "Price: $" +
+                    doughnut.getPrice() + "<br>"  + "Inventory: " +
+                    doughnut.getInventory() + "<br>"  + "<br>" ;
+        }
+        return doughnutView;
     }
 
 }
