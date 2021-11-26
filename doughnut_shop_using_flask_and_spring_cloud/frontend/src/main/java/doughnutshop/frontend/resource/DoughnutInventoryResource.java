ea90517541ad0f6d482df82e246a6b49a31b9218
Iris Moini <azadeh@ucsb.edu>
2021-11-25 16:58:10 -0800

    inital commit of previous microservices

---
+++ doughnut_shop_using_flask_and_spring_cloud/frontend/src/main/java/doughnutshop/frontend/resource/DoughnutInventoryResource.java
@@ -0,0 +1,32 @@
+package doughnutshop.frontend.resource;
+
+import doughnutshop.frontend.model.Doughnut;
+import doughnutshop.frontend.model.DoughnutInventory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doughnuts")
+public class DoughnutInventoryResource {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/")
+    public String getDoughnutInventory(){
+        DoughnutInventory doughnutInventory = restTemplate.getForObject("http://inventory-service-database/doughnuts/orderByPrice", DoughnutInventory.class);
+        List<Doughnut> doughnuts = doughnutInventory.getDoughnutsInventory();
+        String doughnutView = "";
+        for(Doughnut doughnut : doughnuts){
+            doughnutView += "Name: " + doughnut.getName() + "<br>"  + "Price: $" +
+                    doughnut.getPrice() + "<br>"  + "Inventory: " +
+                    doughnut.getInventory() + "<br>"  + "<br>" ;
+        }
+        return doughnutView;
+    }
+
+}
+
---
+++ doughnut_shop_using_flask_and_spring_cloud/inventory-service-database/src/main/java/doughnutshop/inventoryservice/resource/DoughnutInventoryResource.java
@@ -0,0 +1,50 @@
+package doughnutshop.inventoryservice.resource;
+
+import doughnutshop.inventoryservice.model.Doughnut;
+import doughnutshop.inventoryservice.model.DoughnutInventory;
+import doughnutshop.inventoryservice.service.DoughnutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doughnuts")
+public class DoughnutInventoryResource {
+
+    @Autowired
+    private DoughnutService doughnutService;
+
+    @GetMapping(value = "/")
+    public DoughnutInventory getAllDoughnuts() {
+        List<Doughnut> doughnuts = doughnutService.findAll();
+        DoughnutInventory doughnutInventory = new DoughnutInventory(doughnuts);
+        return doughnutInventory;
+    }
+
+    @GetMapping(value = "/byDoughnutName/{doughnutName}")
+    public Doughnut getDoughnutByDoughnutName(@PathVariable("doughnutName") String doughnutName) {
+        return doughnutService.findByName(doughnutName);
+    }
+
+    @GetMapping(value = "/orderByPrice")
+    public DoughnutInventory findAllByOrderByPrice() {
+        List<Doughnut> doughnuts = doughnutService.findAllByOrderByPrice();
+        DoughnutInventory doughnutInventory = new DoughnutInventory(doughnuts);
+        return doughnutInventory;
+    }
+
+    @PostMapping(value = "/save")
+    public ResponseEntity<?> saveOrUpdateDoughnut(@RequestBody Doughnut doughnut) {
+        doughnutService.saveOrUpdateDoughnut(doughnut);
+        return new ResponseEntity("Doughnut added successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{name}")
+    public ResponseEntity<?> deleteDoughnutByName(@PathVariable String name) {
+        doughnutService.deleteDoughnutById(doughnutService.findByName(name).getId());
+        return new ResponseEntity("Doughnut deleted successfully", HttpStatus.OK);
+    }
+}
