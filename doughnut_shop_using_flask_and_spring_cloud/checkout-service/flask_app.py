d3dc1a2638d081b2d10adf77d62cf403d4edfa00
Iris Moini <azadeh@ucsb.edu>
2021-11-25 19:57:46 -0800

    implemented checkout service to decrease inventory of doughnuts by a given quantity

--- doughnut_shop_using_flask_and_spring_cloud/checkout-service/flask_app.py
+++ doughnut_shop_using_flask_and_spring_cloud/checkout-service/flask_app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+import flask
 import py_eureka_client.eureka_client as eureka_client
 import requests
+import urllib
 
 rest_port = 8084
 eureka_client.init(eureka_server="http://localhost:8761/eureka",
@@ -8,9 +10,20 @@ eureka_client.init(eureka_server="http://localhost:8761/eureka",
                    instance_port=rest_port)
 
 app = Flask(__name__)
-@app.route("/", methods=['GET'])
-def get():
-    return "hello world"
+@app.route("/", methods=['POST'])
+def checkout():
+    name = flask.request.json['name']
+    quantity =flask.request.json['quantity']
+    
+    try:
+        res = eureka_client.do_service("inventory-service-database", f"/doughnuts/checkout/{name}/{quantity}")
+        return res
+    except urllib.request.HTTPError as e:
+        # If all nodes are down, a `HTTPError` will raise.
+        return e
+    
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = rest_port)
