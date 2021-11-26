e16d3022f708a072e5c6e6a8f318c5e7e00c3183
Iris Moini <azadeh@ucsb.edu>
2021-11-25 20:44:28 -0800

    added orders endpoint in checkout-service

--- doughnut_shop_using_flask_and_spring_cloud/checkout-service/flask_app.py
+++ doughnut_shop_using_flask_and_spring_cloud/checkout-service/flask_app.py
@@ -10,6 +10,9 @@ eureka_client.init(eureka_server="http://localhost:8761/eureka",
                    instance_port=rest_port)
 
 app = Flask(__name__)
+
+orders_list = []
+
 @app.route("/", methods=['POST'])
 def checkout():
     name = flask.request.json['name']
@@ -17,13 +20,21 @@ def checkout():
     
     try:
         res = eureka_client.do_service("inventory-service-database", f"/doughnuts/checkout/{name}/{quantity}")
+        if res == "Doughnut checked out successfully":
+            orders_list.append(("Doughnut bought: " + name, "Quantity bought: " + str(quantity)))
+            print(orders)
         return res
     except urllib.request.HTTPError as e:
         # If all nodes are down, a `HTTPError` will raise.
         return e
-    
 
-
+@app.route("/orders", methods=['GET'])  
+def orders():
+    output = ""
+    for first, second in orders_list:
+        output = output + first + " " + second + "<br />"
+    print(output)
+    return output
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = rest_port)
